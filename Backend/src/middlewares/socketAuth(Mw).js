e2eaef3b234b10aa703b02ff6858d8e8d9348659
Yuvraj Kumar <yuvrajkumarji1234@gmail.com>
2026-04-26 22:03:45 +0530

    here done one more User spell

--- Backend/src/middlewares/socketAuth(Mw).js
+++ Backend/src/middlewares/socketAuth(Mw).js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import {ENV} from "../lib/env.js";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 export const socketAuthMiddleware = async (socket, next) => {
   try {
