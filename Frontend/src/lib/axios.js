3437baf7d39ba09bca6f25d606f8cc69917d48c7
Yuvraj Kumar <yuvrajkumarji1234@gmail.com>
2026-04-26 22:27:14 +0530

    backend path done

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://opchat.onrender.com/api",
     withCredentials: true,  // ✅ Send cookies with every request
     timeout: 10000,  // 10 second timeout
 });
