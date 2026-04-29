c6875615aa02cae389daa84e31f67869afa1fe9c
Yuvraj Kumar <yuvrajkumarji1234@gmail.com>
2026-04-29 16:51:52 +0530

    utils.js fixed

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,15 +1,22 @@
-import jwt from 'jsonwebtoken';
-import { ENV } from './env.js';
+import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-    const {JWT_SECRET} = ENV;
-    if (!JWT_SECRET) throw new Error('JWT_SECRET is not defined in environment variables');
-    
-    const token = jwt.sign({ userId }, ENV.JWT_SECRET, { expiresIn: '30d' });
-    res.cookie('jwt', token, {
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        httpOnly: true,
-        secure: ENV.NODE_ENV === 'production', // Set secure flag in production
-        sameSite: 'none',
-    });
-}
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: ENV.NODE_ENV === "development" ? false : true,
+  });
+
+  return token;
+};
